225c9f2bf56f45842424ab326335d8df27c374f3
Zach Meyers <zach@zachmeyers.com>
2024-02-01 16:15:25 -0600

    - add tabservice to track active tab in nav bar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,43 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Tab } from '../../models/Tab.model';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { SubSink } from 'subsink';
 import { tap } from 'rxjs';
+import { TabService } from '../../services/tab.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   subs = new SubSink();
   tabs$: Tab[] = [];
-  activeTab: string = '';
 
   constructor(public router: Router,
-    private route: ActivatedRoute) {
-      
-      this.router.events.subscribe(
-        (event: any) => {
-          if (event instanceof NavigationEnd) {
-            this.activeTab = this.router.url.replace('/', '');
-          }
-        }
-      );
+    private tabService: TabService) {
+    
+  }
 
+  ngOnInit(): void {
     this.tabs$ = [
       { title: 'home', route: 'dashboard' },
       { title: 'stations', route: 'stations' },
       { title: 'artists', route: 'artists' }
-    ]
+    ];
+    this.navigate(this.tabs$[0]);
   }
 
   isActiveTab(tab: Tab): boolean {
-    return this.activeTab == tab.route;
+    return this.tabService.activeTab == tab;
   }
 
   navigate(tab: Tab): void {
+    this.tabService.activeTab = tab;
     this.router.navigate([tab.route]);
   }
 
---
+++ src/app/shared/services/tab.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Tab } from '../models/Tab.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TabService {
+
+  activeTab: Tab;
+
+  constructor() { }
+
+}
